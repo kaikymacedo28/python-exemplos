9b7cf6873ee6c533753b495bf8f8a992c87318c3
kaikymacedo28 <kaiky.b.macedo@gmail.com>
2024-04-23 16:41:28 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Kaiky Macedo"
+idade=16
+email="kaikymacedo306@gmail.com"
+telefone="17 99704-5314"
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
